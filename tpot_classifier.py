781f85528a7123f60db64c7c8840c22fb5b63018
ptins <ptinsley@nd.edu>
2018-04-24 18:12:26 -0400

    with undersampling

--- tpot_classifier.py
+++ tpot_classifier.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +11,11 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:0.7984776380538581
-exported_pipeline = RandomForestClassifier(bootstrap=False, criterion="entropy", max_features=0.15000000000000002, min_samples_leaf=14, min_samples_split=4, n_estimators=100)
+# Score on the training set was:0.786460331469691
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.7000000000000001, min_samples_leaf=2, min_samples_split=3, n_estimators=100)),
+    RandomForestClassifier(bootstrap=True, criterion="entropy", max_features=0.9000000000000001, min_samples_leaf=19, min_samples_split=11, n_estimators=100)
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
